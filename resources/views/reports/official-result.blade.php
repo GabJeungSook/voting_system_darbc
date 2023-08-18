5baf61203425c8c751abf4026c3bff32875a46f2
GabJeungSook <gicawalo@gmail.com>
2023-08-18 11:53:08 +0800

    testing sort by rank

--- resources/views/reports/official-result.blade.php
+++ resources/views/reports/official-result.blade.php
@@ -25,15 +25,18 @@
                         <th  class="bg-indigo-800 py-2 pl-4 pr-3 text-left text-sm uppercase font-semibold text-white sm:pl-3 border-r">{{$item->name}}</th>
                         <th  class="bg-indigo-800 py-2 pl-4 pr-3 text-left text-sm uppercase font-semibold text-white sm:pl-3">Total Votes</th>
                     </tr>
+                    @php
+                    $candidatesSorted = $item->candidates->sortByDesc('vote_count');
+                    @endphp
                     @foreach ($item->candidates->sortBy('last_name')->take($item->number_of_winners) as $candidate)
                     {{-- @foreach ($item->candidates->sortByDesc('vote_count')->take($item->number_of_winners) as $candidate) --}}
                     <tr class="border-t border-gray-600">
                         <td class="whitespace-nowrap py-2 pl-4 pr-3 text-left text-sm font-medium text-gray-900 sm:pl-3 border-r">
                             {{$counter++}}
                          </td>
-                         {{-- <td class="whitespace-nowrap py-2 pl-4 text-left text-sm font-medium text-gray-900 sm:pl-3 border-r">
-                            {{$loop->iteration}}
-                        </td> --}}
+                         <td class="whitespace-nowrap py-2 pl-4 text-left text-sm font-medium text-gray-900 sm:pl-3 border-r">
+                            {{$candidatesSorted->search($candidate) + 1}}
+                        </td>
                         <td class="whitespace-nowrap py-2 pl-4 pr-3 text-left text-sm font-medium text-gray-900 sm:pl-3 border-r">
                             {{strtoupper($candidate->last_name.', '.$candidate->first_name.' '.$candidate->middle_name)}}
                         </td>
