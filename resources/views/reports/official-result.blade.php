9c71b6c4889dac1a78b43dea514f77e6099ff933
GabJeungSook <gicawalo@gmail.com>
2023-08-18 11:45:14 +0800

    try again last_name

--- resources/views/reports/official-result.blade.php
+++ resources/views/reports/official-result.blade.php
@@ -24,7 +24,7 @@
                         <th  class="bg-indigo-800 py-2 pl-4 pr-3 text-left text-sm uppercase font-semibold text-white sm:pl-3 border-r">{{$item->name}}</th>
                         <th  class="bg-indigo-800 py-2 pl-4 pr-3 text-left text-sm uppercase font-semibold text-white sm:pl-3">Total Votes</th>
                     </tr>
-                    @foreach ($item->candidates->sortByAsc('last_name')->take($item->number_of_winners) as $candidate)
+                    @foreach ($item->candidates->sortBy('last_name')->take($item->number_of_winners) as $candidate)
                     {{-- @foreach ($item->candidates->sortByDesc('vote_count')->take($item->number_of_winners) as $candidate) --}}
                     <tr class="border-t border-gray-600">
                         <td class="whitespace-nowrap py-2 pl-4 pr-3 text-left text-sm font-medium text-gray-900 sm:pl-3 border-r">
