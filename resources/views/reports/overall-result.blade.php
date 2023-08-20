9dc0e0b4788d26bda7c90b078fa375aa811740a2
GabJeungSook <gicawalo@gmail.com>
2023-08-20 15:21:43 +0800

    test ranking on table

--- resources/views/reports/overall-result.blade.php
+++ resources/views/reports/overall-result.blade.php
@@ -18,30 +18,34 @@
                     @foreach ($positions as $item)
                     @php
                         $counter = 1;
+                        $previous_vote_count = PHP_INT_MAX; // Initialize with a large value
+                        $rank = 1;
                     @endphp
                     <tr class="border-t border-gray-600">
-                        <th  class="bg-indigo-800 py-2 pl-4 pr-3 text-left text-sm uppercase font-semibold text-white sm:pl-3 border-r">No.</th>
-                        {{-- <th class="bg-indigo-800 py-2 pl-4 pr-3 text-left text-sm uppercase font-semibold text-white sm:pl-3">Rank</th> --}}
-                        <th  class="bg-indigo-800 py-2 pl-4 pr-3 text-left text-sm uppercase font-semibold text-white sm:pl-3 border-r">{{$item->name}}</th>
-                        <th  class="bg-indigo-800 py-2 pl-4 pr-3 text-left text-sm uppercase font-semibold text-white sm:pl-3">Total Votes</th>
+                        <th class="bg-indigo-800 py-2 pl-4 pr-3 text-left text-sm uppercase font-semibold text-white sm:pl-3 border-r">No.</th>
+                        <th class="bg-indigo-800 py-2 pl-4 pr-3 text-left text-sm uppercase font-semibold text-white sm:pl-3 border-r">{{$item->name}}</th>
+                        <th class="bg-indigo-800 py-2 pl-4 pr-3 text-left text-sm uppercase font-semibold text-white sm:pl-3">Total Votes</th>
+                        <th class="bg-indigo-800 py-2 pl-4 pr-3 text-left text-sm uppercase font-semibold text-white sm:pl-3">Rank</th>
                     </tr>
-                    {{-- @php
-                    $candidatesSorted = $item->candidates->sortByDesc('vote_count');
-                    @endphp --}}
                     @foreach ($item->candidates->sortBy('last_name') as $candidate)
-                    {{-- @foreach ($item->candidates->sortByDesc('vote_count') as $candidate) --}}
                     <tr class="border-t border-gray-600">
                         <td class="whitespace-nowrap py-2 pl-4 pr-3 text-left text-sm font-medium text-gray-900 sm:pl-3 border-r">
-                           {{$counter++}}
+                            {{$counter++}}
                         </td>
-                        {{-- <td class="whitespace-nowrap py-2 pl-4 text-left text-sm font-medium text-gray-900 sm:pl-3 border-r">
-                            {{$candidatesSorted->search($candidate) + 1}}
-                        </td> --}}
                         <td class="whitespace-nowrap py-2 pl-4 pr-3 text-left text-sm font-medium text-gray-900 sm:pl-3 border-r">
                             {{strtoupper($candidate->last_name.', '.$candidate->first_name.' '.$candidate->middle_name)}}
                         </td>
-                        <td class="whitespace-nowrap py-2 pl-4 text-left text-sm font-medium text-gray-900 sm:pl-3 border-r">
+                        <td class="whitespace-nowrap py-2 pl-4 pr-3 text-left text-sm font-medium text-gray-900 sm:pl-3 border-r">
                             {{$candidate->vote_count}}
+                        </td>
+                        <td class="whitespace-nowrap py-2 pl-4 pr-3 text-left text-sm font-medium text-gray-900 sm:pl-3">
+                            @if ($candidate->vote_count < $previous_vote_count)
+                                {{$rank}}
+                                @php $rank++; @endphp
+                            @else
+                                {{$rank}}
+                            @endif
+                            @php $previous_vote_count = $candidate->vote_count; @endphp
                         </td>
                     </tr>
                     @endforeach
