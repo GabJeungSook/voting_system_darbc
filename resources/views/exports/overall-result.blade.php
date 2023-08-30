9e98beac630da0c93abbff8bf08526118bb5f378
GabJeungSook <gicawalo@gmail.com>
2023-08-30 10:13:42 +0800

    changes in export

--- resources/views/exports/overall-result.blade.php
+++ resources/views/exports/overall-result.blade.php
@@ -2,17 +2,26 @@
     <tbody class="bg-white">
         @foreach ($positions as $item)
         @php
-            $counter = 1;
+            $voteCounts = $item->candidates->pluck('vote_count')->toArray();
+            rsort($voteCounts); // Sort vote counts in descending order
+            $rank = 1;
         @endphp
         <tr class="border-t border-gray-600">
-            <th  class="bg-indigo-800 py-2 pl-4 pr-3 text-left text-sm uppercase font-semibold text-white sm:pl-3 border-r">No.</th>
-            <th  class="bg-indigo-800 py-2 pl-4 pr-3 text-left text-sm uppercase font-semibold text-white sm:pl-3 border-r">{{$item->name}}</th>
-            <th  class="bg-indigo-800 py-2 pl-4 pr-3 text-left text-sm uppercase font-semibold text-white sm:pl-3">Total Votes</th>
+            <th class="bg-indigo-800 py-2 pl-4 pr-3 text-left text-sm uppercase font-semibold text-white sm:pl-3 border-r">No.</th>
+            <th class="bg-indigo-800 py-2 pl-4 pr-3 text-left text-sm uppercase font-semibold text-white sm:pl-3">Rank</th>
+            <th class="bg-indigo-800 py-2 pl-4 pr-3 text-left text-sm uppercase font-semibold text-white sm:pl-3 border-r">{{$item->name}}</th>
+            <th class="bg-indigo-800 py-2 pl-4 pr-3 text-left text-sm uppercase font-semibold text-white sm:pl-3">Total Votes</th>
         </tr>
         @foreach ($item->candidates->sortBy('last_name') as $candidate)
         <tr class="border-t border-gray-600">
             <td class="whitespace-nowrap py-2 pl-4 pr-3 text-left text-sm font-medium text-gray-900 sm:pl-3 border-r">
-               {{$counter++}}
+                {{$loop->iteration}}
+            </td>
+            <td class="whitespace-nowrap py-2 pl-4 pr-3 text-left text-sm font-medium text-gray-900 sm:pl-3 border-r">
+                @php
+                    $voteIndex = array_search($candidate->vote_count, $voteCounts);
+                    echo $voteIndex !== false ? $voteIndex + 1 : '';
+                @endphp
             </td>
             <td class="whitespace-nowrap py-2 pl-4 pr-3 text-left text-sm font-medium text-gray-900 sm:pl-3 border-r">
                 {{strtoupper($candidate->last_name.', '.$candidate->first_name.' '.$candidate->middle_name)}}
@@ -23,6 +32,6 @@
         </tr>
         @endforeach
         @endforeach
-        <!-- More people... -->
     </tbody>
 </table>
+
