b9c31dde66ee69feb9b60afff16bab0c4cb1852a
Gabriel Jon Icawalo <67619600+GabJeungSook@users.noreply.github.com>
2024-01-16 10:03:30 +0800

    s

--- resources/views/reports/voided-votes.blade.php
+++ resources/views/reports/voided-votes.blade.php
@@ -36,7 +36,8 @@
                         </td>
                         <td class="whitespace-nowrap py-2 pl-4 text-left text-sm font-medium text-gray-900 sm:pl-3 border-r">
                             {{-- {{strtoupper($item->user->name)}} --}}
-                            {{strtoupper($item->registered_member->vote->user)}}
+                            @dump($item->registered_member->vote)
+                            {{strtoupper($item->registered_member->vote)}}
                         </td>
                         <td class="whitespace-nowrap py-2 pl-4 text-left text-sm font-medium text-gray-900 sm:pl-3 border-r">
                             {{strtoupper($item->note)}}
