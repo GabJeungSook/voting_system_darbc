435b9148477374f75aefdb21275d6075caa74d11
Gabriel Jon Icawalo <67619600+GabJeungSook@users.noreply.github.com>
2024-01-16 10:42:17 +0800

    waa

--- app/Http/Livewire/Admin/Reports/VoterList.php
+++ app/Http/Livewire/Admin/Reports/VoterList.php
@@ -48,6 +48,10 @@ class VoterList extends Component
             $query->wherehas('vote.user', function($query){
                 $query->where('id', $this->selectedCounter);
             });
+        })->when(!empty($this->member_name) , function ($query) {
+            $query->where('first_name', 'like', '%'.$this->member_name.'%')
+            ->orWhere('middle_name', 'like', '%'.$this->member_name.'%')
+            ->orWhere('last_name', 'like', '%'.$this->member_name.'%');
         })
         ->with(['registration_duration','vote.user'])->get();
 
